2e01f1a037554f3e3b908bff0fbb54986dfcb774
kazitanviranjum <96996966+kazitanviranjum@users.noreply.github.com>
2022-02-25 23:44:35 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function (){
+    
+//pre-lodar
+    $(window).on("load", function () {
+        $('#loding').fadeOut(5000);
+    })
+//navication bar
+    $('a').on('click', function (e) {
+        e.preventDefault()
+
+        var hash = this.hash
+
+        if (this.hash !== '') {
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top
+                },
+                500
+            )
+        }
+    })
+
+//arrow 
+    $(window).on('scroll', function () {
+        var scrollTop = $(this).scrollTop();
+        
+        if (scrollTop > 600) {
+            $('#top').fadeIn(300)
+        } else {
+            $('#top').fadeOut(300)
+        }
+    })
+
+    $('#top').on('click', function () {
+        $("html, body").animate({
+            scrollTop:0
+        },1000)
+    })
+})
